707c2fa89c1e69be93faa16859f71bb4c1a06adb
Alihtt <Aliht.workspace@gmail.com>
2024-02-10 20:10:57 +0330

    thread subclass

--- show.py
+++ show.py
@@ -4,15 +4,25 @@ from threading import Thread
 start = perf_counter()
 
 
-def show(name):
+def show(name, delay):
     print(f'Starting {name}...')
-    sleep(3)
+    sleep(delay)
     print(f'Finished {name}.')
 
 
+class ShowThread(Thread):
+    def __init__(self, name, delay):
+        super().__init__()
+        self.name = name
+        self.delay = delay
+
+    def run(self):
+        show(self.name, self.delay)
+
+
 # Creating threads instance
-th1 = Thread(target=show, args=('One',))
-th2 = Thread(target=show, args=('Two',))
+th1 = ShowThread('One', 3)
+th2 = ShowThread('Two', 7)
 
 # Starting threads
 th1.start()
