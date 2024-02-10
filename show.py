64df2af2b13335e1c5c4597ac99e57a4299821ab
Alihtt <Aliht.workspace@gmail.com>
2024-02-10 21:11:15 +0330

    current_thread & enumerate

--- show.py
+++ show.py
@@ -1,33 +1,30 @@
 from time import sleep, perf_counter
-from threading import Thread
-import sys
+from threading import Thread, current_thread, enumerate
 
 start = perf_counter()
 
 
 def show(name, delay):
     print(f'Starting {name}...')
+
+    print(current_thread())  # Return the current thread
+    print(enumerate())  # Return all alive threads
+    
     sleep(delay)
     print(f'Finished {name}.')
 
 
 # Creating threads instance
-th1 = Thread(target=show, args=('One', 3), daemon=True)
-th2 = Thread(target=show, args=('Two', 7), daemon=True)
+th1 = Thread(name='First', target=show, args=('One', 3))
+th2 = Thread(name='Second', target=show, args=('Two', 7))
 
 # Starting threads
 th1.start()
 th2.start()
 
+# Waiting until threads are finished
+th1.join()
+th2.join()
+
 end = perf_counter()
 print(round(end - start))
-
-"""
-daemon note
-
-if daemon == True:
-    print('the thread does not wait for the answer and run sys.exit()')
-else:
-    print('the thread waits for the answer and after receiving the answer runs sys.exit()')
-"""
-sys.exit()
