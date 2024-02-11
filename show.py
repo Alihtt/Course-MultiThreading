a10483249078c1f63f1b6ae8155f28966aa4fbf5
Alihtt <Aliht.workspace@gmail.com>
2024-02-11 15:37:27 +0330

    event

--- show.py
+++ show.py
@@ -1,9 +1,35 @@
-from threading import Timer
+from threading import Thread, Event
+from time import sleep
 
 
-def show():
-    print('how u doing buddy?')
+def first(f, s):
+    sleep(10)
+    print('First is ready ... ')
+    f.set()
+    s.wait()
+    print('First is working ... ')
+    f.clear()
 
 
-t = Timer(10, show)  # Our function will run after 10 seconds
-t.start()
+def second(f, s):
+    print('Second is ready ... ')
+    s.set()
+    f.wait()
+    print('Second is working ... ')
+    s.clear()
+
+
+"""
+This is one of the simplest mechanisms for communication between threads: one thread signals an event and other threads wait for it.
+
+https://docs.python.org/3/library/threading.html#event-objects
+"""
+
+f = Event()
+s = Event()
+
+th1 = Thread(target=first, args=(f, s))
+th2 = Thread(target=second, args=(f, s))
+
+th1.start()
+th2.start()
